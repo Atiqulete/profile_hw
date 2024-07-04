f8b1de4ffa972930c974bf8cbb2af855a6023c9b
unknown <atiqulete@gmail.com>
2024-07-04 13:01:22 +0600

    Profile_v.1

--- Profile/website/views.py
+++ Profile/website/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
+from .models import userInfo,Experience
 
 # Create your views here.
 
 
 def home(request):
-    return render(request,'index.html')
+    user_info = userInfo.objects.all()
+    context = {
+        'user_info' : user_info
+    }
+    return render(request,'index.html',context)
+
 def contact(request):
     return render(request,'contact.html')
+
 def projects(request):
     return render(request,'projects.html')
+
+
 def resume(request):
-    return render(request,'resume.html')
+    ex_info = Experience.objects.all()
+    context = {
+        'ex_info' : ex_info
+    }
+    return render(request,'resume.html',context,)
+
